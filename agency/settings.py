5b5b4f07ef1c624b227db1f41f94146919ba450b
koya adegboyega <koyexes@gmail.com>
2024-03-28 09:54:14 +0100

    Add heroku host to allowed_host in settings.py

--- agency/settings.py
+++ agency/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-(o%z2&6zi^^!+482h9q9c006oxse^pbr9&plg+8+_f8!ze^ljb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env("HOST")]
 
 # Application definition
 
